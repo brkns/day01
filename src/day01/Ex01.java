2302c64e290a9855e7b14d3531d25e34f0c3be78
dukHyun <boorycoon@gmaii.com>
2022-03-16 14:41:39 +0900

    작업

---
+++ src/day01/Ex01.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+		/*
+		 * \n 엔터
+		 * \t tab크기만큼 오른쪽으로 이동
+		 * \" 큰따옴표 표현
+		 * \\ 역슬래시 표현
+		*/
+		System.out.println("안\n녕\n하\n세\n요");
+		System.out.println("1\t123\t1234567\t11");
+		System.out.println("  \"  \\ ");
+	}
+}
+
---
+++ src/day01/Ex02.java
@@ -0,0 +1,51 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		System.out.println("======================================");
+		System.out.println("이름" +"\t"+ "나이"+"\t"+"전화번호" + "\t" +"\t"+ "회비");
+		System.out.println("======================================");
+		System.out.print("홍길동"+"\t");
+		System.out.print("\"");
+		System.out.print(15);
+		System.out.print("\"");
+		System.out.print("\t");
+		System.out.print(3672);
+		System.out.print("-");
+		System.out.print(1234);
+		System.out.print("\t");
+		System.out.print("\\");
+		System.out.println(20000);
+		System.out.print("고길동"+"\t");
+		System.out.print("\"");
+		System.out.print(15);
+		System.out.print("\"");
+		System.out.print("\t");
+		System.out.print(2238);
+		System.out.print("-");
+		System.out.print(1234);
+		System.out.print("\t");
+		System.out.print("\\");
+		System.out.println(30000);
+		System.out.print("김말이"+"\t");
+		System.out.print("\"");
+		System.out.print(15);
+		System.out.print("\"");
+		System.out.print("\t");
+		System.out.print(1234);
+		System.out.print("-");
+		System.out.print(1234);
+		System.out.print("\t");
+		System.out.print("\\");
+		System.out.println(50000);
+		System.out.println("--------------------------------------");
+		System.out.print("총합계");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.println("\\100000");
+		System.out.println("--------------------------------------");
+		
+	}
+}
